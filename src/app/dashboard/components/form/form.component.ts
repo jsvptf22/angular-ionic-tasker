56a331b8331536479ad0a11c86520c3b25455b54
jhon sebastian valencia <sebasjsv97@gmail.com>
2019-06-10 20:10:23 -0500

    validaciones en formulario de tareas

--- src/app/dashboard/components/form/form.component.ts
+++ src/app/dashboard/components/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { SaveEditService } from '../../services/save-edit.service';
 import { TasksService } from '../../services/tasks.service';
 import { Storage } from '@ionic/storage';
@@ -11,9 +12,12 @@ import { Storage } from '@ionic/storage';
 export class FormComponent implements OnInit {
   @Output() toogleForm = new EventEmitter<boolean>();
 
-  id: number;
-  name: string;
-  description: string;
+  taskForm = new FormGroup({
+    id: new FormControl(''),
+    name: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+  });
+  checkForm: boolean;
 
   constructor(
     private storage: Storage,
@@ -32,27 +36,35 @@ export class FormComponent implements OnInit {
   getActive() {
     this.saveEditService.getActive().subscribe((task: any) => {
       if (task.id) {
-        this.id = task.id;
-        this.name = task.name;
-        this.description = task.description;
+        this.taskForm.controls.id.setValue(task.id);
+        this.taskForm.controls.name.setValue(task.name);
+        this.taskForm.controls.description.setValue(task.description);
       } else {
-        this.id = null;
-        this.name = null;
-        this.description = null;
+        this.taskForm.reset();
       }
     });
   }
 
   save() {
+    this.checkForm = true;
+
+    if (!this.taskForm.valid) {
+      return false;
+    }
+
     this.saveEditService
-      .save(this.id, this.name, this.description)
+      .save(
+        this.taskForm.value.id,
+        this.taskForm.value.name,
+        this.taskForm.value.description
+      )
       .subscribe((response: any) => {
         if (response.id) {
-          if (this.id) {
-            this.tasksService.updateTask(this.id, response);
+          if (this.taskForm.value.id) {
+            this.tasksService.updateTask(this.taskForm.value.id, response);
           } else {
             this.tasksService.add(response);
-            this.name = this.description = '';
+            this.taskForm.reset();
           }
           this.afterSave();
         }
@@ -67,4 +79,8 @@ export class FormComponent implements OnInit {
   cancel() {
     this.afterSave();
   }
+
+  get id() { return this.taskForm.get('id'); }
+  get name() { return this.taskForm.get('name'); }
+  get description() { return this.taskForm.get('description'); }
 }
